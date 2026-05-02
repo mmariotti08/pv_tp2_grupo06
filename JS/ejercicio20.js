5e80ecd780140394932b98ae98027904ec399110
unknown <benjaminagustinortega@gmail.com>
2026-05-02 15:25:52 -0300

    ejercicio20 benjaminOrtega

--- JS/ejercicio20.js
+++ JS/ejercicio20.js
@@ -1,11 +1,21 @@
+import { obtenerDatos } from "./funciones20.js";
+//import { servicioBenjaminOrtega } from "../services/serviceBenjaminOrtega.js";
+//import { servicioGonzaloPeñalva } from "../services/servicesGonzaloPeñalva.js";
+//import { servicioMarcosSaldaño } from "../services/serviceMarcosSaldaño";
+//import { servicioMatiasMariotti } from "../services/serviceMatiasMariotti.js";
+//import { servicioDiegoCari } from "../services/serviceDiegoCari.js";
 
+const nombreInput = document.querySelector("#nombre");
+const apellidoInput = document.querySelector("#apellido");
+const libretaInput = document.querySelector("#libreta");
+const boton = document.querySelector("#btn");
 
-import { servicioBenjaminOrtega } from "../services/serviceBenjaminOrtega.js";
+boton.addEventListener("click", () => {
+    const nombre = nombreInput.value;
+    const apellido = apellidoInput.value;
+    const libreta = libretaInput.value;
 
-import { servicioGonzaloPeñalva } from "../services/servicesGonzaloPeñalva.js";
+    const resultado = obtenerDatos(nombre, apellido, libreta);
 
-import { servicioMarcosSaldaño } from "../services/serviceMarcosSaldaño";
-
-import { servicioMatiasMariotti } from "../services/serviceMatiasMariotti.js";
-
-import { servicioDiegoCari } from "../services/serviceDiegoCari.js";
+    alert(resultado);
+});
