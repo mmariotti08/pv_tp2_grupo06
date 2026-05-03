b543d55d2aa56faf2e569318c4c9abaead4e4ea4
diegou777 <caridiego11@gmail.com>
2026-05-03 17:52:23 -0300

    Ejercicio 24 terminado con eventos y modulos

---
+++ JS/funciones24.js
@@ -0,0 +1,4 @@
+export const mostrarSeleccion = (valorSeleccionado, resultado) => {
+    resultado.textContent = `Lenguaje seleccionado: ${valorSeleccionado}`;
+    console.log(`Lenguaje seleccionado: ${valorSeleccionado}`);
+};
